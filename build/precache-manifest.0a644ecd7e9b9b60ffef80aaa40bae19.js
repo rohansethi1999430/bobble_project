d6d2a488ac0f9cdad52ee8ed06a35355a0e3b145
Rohan <rohansethi1999430@icloud.com>
2020-10-10 19:10:23 +0530

    Build Folder added

---
+++ build/precache-manifest.0a644ecd7e9b9b60ffef80aaa40bae19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5502d5fc39207bc3b179b5fd54595d0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf5ce26163201cc3370",
+    "url": "/static/css/main.3e341ce7.chunk.css"
+  },
+  {
+    "revision": "c233dff12cade89d48f3",
+    "url": "/static/js/2.c8a3cb63.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.c8a3cb63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf5ce26163201cc3370",
+    "url": "/static/js/main.59429931.chunk.js"
+  },
+  {
+    "revision": "8177a49bd3f85060c299",
+    "url": "/static/js/runtime-main.183362d0.js"
+  },
+  {
+    "revision": "a12d55df343f808e95e2f3e34266e84c",
+    "url": "/static/media/Logo.a12d55df.png"
+  }
+]);
